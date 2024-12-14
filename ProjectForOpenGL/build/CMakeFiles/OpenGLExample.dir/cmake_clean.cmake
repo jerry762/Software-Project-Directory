5f8effca387a076f89de62fc3de7cc0776396d89
Liu kuan-Tsen <75406330+jerry762@users.noreply.github.com>
2024-12-14 10:35:07 +0800

    Add OpenGL build dir sample

---
+++ ProjectForOpenGL/build/CMakeFiles/OpenGLExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLExample.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGLExample.dir/src/test.cpp.o"
+  "OpenGLExample"
+  "OpenGLExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
